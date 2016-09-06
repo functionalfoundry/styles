a6ae2afa2e7c1366ba41d2900cecf47f2eee5dfb
Yaniv Tal <yaniv@workfloapp.com>
2016-09-06 13:14:12 -0700

    feature(Corners): Add Corners variables

---
+++ src/Corners.js
@@ -0,0 +1,10 @@
+const Corners = {
+  square: {
+    borderRadius: 0,
+  },
+  round: {
+    borderRadius: 3,
+  },
+}
+
+export default Corners
---
+++ src/Corners.story.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import Borders from './Borders'
+import Colors from './Colors'
+import Corners from './Corners'
+import Fonts from './Fonts'
+
+type CornerPropsT = {
+  cornerStyle: Object,
+  label: string,
+}
+
+const Corner = ({
+  cornerStyle,
+  label,
+}: CornerPropsT) => (
+  <div>
+    <div
+      style={{
+        ...Borders.light,
+        ...cornerStyle,
+        height: 64,
+        marginBottom: 8,
+        marginTop: 24,
+        backgroundColor: 'white',
+      }}
+    />
+    <div
+      style={{
+        ...Fonts.base,
+      }}
+    >
+      {label}
+    </div>
+  </div>
+)
+
+storiesOf('Corners', module)
+  .add('Standard', () => (
+    <div
+      style={{
+        backgroundColor: Colors.grey1200,
+        padding: 16,
+      }}
+    >
+      {Object.keys(Corners).map((corner) => (
+        <Corner
+          key={corner}
+          cornerStyle={Corners[corner]}
+          label={corner}
+        />
+      ))}
+    </div>
+  ))
