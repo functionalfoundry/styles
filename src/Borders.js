076aec3ddf0f3a9887496467747d6e45d54b6b91
Yaniv Tal <yaniv@workfloapp.com>
2016-09-06 13:13:56 -0700

    feature(Borders): Add Borders variables

---
+++ src/Borders.js
@@ -0,0 +1,18 @@
+import Colors from './Colors'
+
+const Borders = {
+  none: {
+    border: 0,
+  },
+  light: {
+    border: `1px solid ${Colors.grey1100}`,
+  },
+  base: {
+    border: `1px solid ${Colors.grey1000}`,
+  },
+  dark: {
+    border: `1px solid ${Colors.grey700}`,
+  },
+}
+
+export default Borders
---
+++ src/Borders.story.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import Borders from './Borders'
+import Fonts from './Fonts'
+
+type BorderPropsT = {
+  borderStyle: Object,
+  label: string,
+}
+
+const Border = ({
+  borderStyle,
+  label,
+}: BorderPropsT) => (
+  <div>
+    <div
+      style={{
+        ...borderStyle,
+        height: 64,
+        marginBottom: 8,
+        marginTop: 24,
+      }}
+    />
+    <div
+      style={{
+        ...Fonts.base,
+      }}
+    >
+      {label}
+    </div>
+  </div>
+)
+
+storiesOf('Borders', module)
+  .add('Standard', () => (
+    <div>
+      {Object.keys(Borders).map((border) => (
+        <Border
+          key={border}
+          borderStyle={Borders[border]}
+          label={border}
+        />
+      ))}
+    </div>
+  ))
