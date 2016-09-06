05bf4cc5077ddf254e1daffa4d8848deb25c99ce
Yaniv Tal <yaniv@workfloapp.com>
2016-09-06 10:54:13 -0700

    feature(Colors): Add standard Colors variables

---
+++ src/Colors.js
@@ -0,0 +1,23 @@
+const Colors = {
+  primary: '#0BD8D8',
+  primaryDark: '#00B5B5',
+  secondary: '#2b3842',
+  success: '#00D460',
+  warn: '#FFE626',
+  danger: '#F24965',
+  grey100: '#0F1619',
+  grey200: '#212D33',
+  grey300: '#293840',
+  grey400: '#39464C',
+  grey500: '#525F66',
+  grey600: '#707A80',
+  grey700: '#879399',
+  grey800: '#A1ADB2',
+  grey900: '#BCC7CC',
+  grey1000: '#D3D8DB',
+  grey1100: '#E1E7EB',
+  grey1200: '#F5F8FA',
+  white: '#FFFFFF',
+}
+
+export default Colors
