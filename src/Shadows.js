8d29b6b6b1dc0c95f98442c0ba44b31920ce9d78
Yaniv Tal <yaniv@workfloapp.com>
2016-09-12 12:01:00 -0700

    feature(Shadows): Add shadows variables

---
+++ src/Shadows.js
@@ -0,0 +1,32 @@
+const lengths = {
+  micro: '1px',
+  tiny: '2px',
+  small: '4px',
+  base: '12px',
+  large: '18px',
+  huge: '28px',
+}
+
+const colors = {
+  light: 'rgba(0,0,0,0.25)',
+  dark: 'rgba(0,0,0,0.22)',
+}
+
+const getDimension = (length, color) => `0 ${length} ${length} ${color}`
+
+const Shadows = {
+  small: {
+    boxShadow: `${getDimension(lengths.small, colors.light)}, ${getDimension(lengths.small, colors.dark)}`,
+  },
+  base: {
+    boxShadow: `${getDimension(lengths.base, colors.light)}, ${getDimension(lengths.small, colors.dark)}`,
+  },
+  large: {
+    boxShadow: `${getDimension(lengths.large, colors.light)}, ${getDimension(lengths.base, colors.dark)}`,
+  },
+  huge: {
+    boxShadow: `${getDimension(lengths.huge, colors.light)}, ${getDimension(lengths.base, colors.dark)}`,
+  },
+}
+
+export default Shadows
---
+++ src/Shadows.story.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import Colors from './Colors'
+import Fonts from './Fonts'
+import Shadows from './Shadows'
+
+type ShadowPropsT = {
+  fontStyle: Object,
+  shadowStyle: Object,
+  label: string,
+}
+
+const Shadow = ({
+  fontStyle,
+  shadowStyle,
+  label,
+}: ShadowPropsT) => (
+  <div
+    style={{
+      display: 'inline-block',
+    }}
+  >
+    <div
+      style={{
+        ...fontStyle,
+      }}
+    >
+      {label}
+    </div>
+    <div
+      style={{
+        ...shadowStyle,
+        backgroundColor: 'white',
+        width: 220,
+        height: 320,
+        margin: 20,
+      }}
+    />
+  </div>
+)
+
+storiesOf('Shadows', module)
+  .add('Standard', () => (
+    <div
+      style={{
+        backgroundColor: Colors.grey1200,
+      }}
+    >
+      {Object.keys(Shadows).map((shadow) => (
+        <Shadow
+          key={shadow}
+          fontStyle={Fonts.base}
+          shadowStyle={Shadows[shadow]}
+          label={shadow}
+        />
+      ))}
+    </div>
+  ))
