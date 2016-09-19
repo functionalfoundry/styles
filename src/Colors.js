75fb88413a15b7e5682e909542bf41c1ead41ee7
Yaniv Tal <yaniv@workfloapp.com>
2016-09-19 11:09:57 -0700

    feature(Colors): Update colors

--- src/Colors.js
+++ src/Colors.js
@@ -1,23 +1,88 @@
+const defaultGrey = '#0f1619'
+const defaultPrimary = '#0be5e5'
+const defaultGreen = '#0BD466'
+const defaultYellow = '#FFE626'
+const defaultRed = '#F24965'
+
+const Greys = {
+  grey: defaultGrey,
+  grey50: '#fdfdfe',
+  grey100: '#f5f8fa',
+  grey200: '#d8dcde',
+  grey300: '#bcc0c2',
+  grey400: '#9fa3a6',
+  grey500: '#9fa3a6',
+  grey600: '#656b6e',
+  grey700: '#494f52',
+  grey800: '#2c3236',
+  grey900: defaultGrey,
+}
+
+const Primaries = {
+  primary: defaultPrimary,
+  primary50: '#f3fefe',
+  primary100: '#d4fafa',
+  primary200: '#91f3f3',
+  primary300: '#4eecec',
+  primary400: defaultPrimary,
+  primary500: '#09cccc',
+  primary600: '#07b3b3',
+  primary700: '#059999',
+  primary800: '#028080',
+  primary900: '#006666',
+}
+
+const Greens = {
+  green: defaultGreen,
+  success: defaultGreen,
+  green50: '#F2FEF7',
+  green100: '#D5FAE5',
+  green200: '#92EDBB',
+  green300: '#4EE190',
+  green400: defaultGreen,
+  green500: '#11B95D',
+  green600: '#169E54',
+  green700: '#1C834A',
+  green800: '#216841',
+  green900: '#274D38',
+}
+
+const Yellows = {
+  yellow: defaultYellow,
+  warning: defaultYellow,
+  yellow50: '#FFFDF2',
+  yellow100: '#FFFBD9',
+  yellow200: '#FFF49D',
+  yellow300: '#FFED62',
+  yellow400: defaultYellow,
+  yellow500: '#DBC626',
+  yellow600: '#B8A727',
+  yellow700: '#948827',
+  yellow800: '#716827',
+  yellow900: '#4D4927',
+}
+
+const Reds = {
+  red: defaultRed,
+  danger: defaultRed,
+  red50: '#FFF0F2',
+  red100: '#FFD9DF',
+  red200: '#FBA9B6',
+  red300: '#F6798E',
+  red400: defaultRed,
+  red500: '#D6455D',
+  red600: '#BA4054',
+  red700: '#9E3C4C',
+  red800: '#823743',
+  red900: '#66333B',
+}
+
 const Colors = {
-  primary: '#0BD8D8',
-  primaryHover: '#00B5B5',
-  secondary: '#2b3842',
-  success: '#00D460',
-  warn: '#FFE626',
-  danger: '#F24965',
-  grey100: '#0F1619',
-  grey200: '#212D33',
-  grey300: '#293840',
-  grey400: '#39464C',
-  grey500: '#525F66',
-  grey600: '#707A80',
-  grey700: '#879399',
-  grey800: '#A1ADB2',
-  grey900: '#BCC7CC',
-  grey1000: '#D3D8DB',
-  grey1100: '#E1E7EB',
-  grey1200: '#F5F8FA',
-  white: '#FFFFFF',
+  ...Greys,
+  ...Primaries,
+  ...Greens,
+  ...Yellows,
+  ...Reds,
 }
 
 export default Colors
